d54ca9be9b319532fa38ef1b2bace558c436d435
stephenmwilkins <s.wilkins@sussex.ac.uk>
2025-11-16 16:07:39 +0000

    updated

--- dev/test_buttons-12x10.py
+++ dev/test_buttons-12x10.py
@@ -1,6 +1,7 @@
 
 import board
 import keypad
+import neopixel
 
 def key_to_pixel_map(key_number, cols):
     row = key_number // cols
@@ -11,21 +12,13 @@ def key_to_pixel_map(key_number, cols):
 cols = 12
 rows = 10
 
-
+npixels = cols * rows
+pixels = neopixel.NeoPixel(board.D18, npixels, brightness=1)
 
 
 row_pins = (board.D26, board.D19, board.D13, board.D6, board.D5, board.D0, board.D11, board.D9, board.D10, board.D22)
-# row_pins = (board.D26, board.D19)
 column_pins = (board.D14, board.D15, board.D23, board.D24, board.D25, board.D12, board.D16, board.D20, board.D21, board.D27, board.D17, board.D4)
-# column_pins = (board.D14, board.D15)
 
-
-cols = len(column_pins)
-rows = len(row_pins)
-
-
-print(len(row_pins))
-print(len(column_pins))
 
 # THIS NEEDS TO BE UPDATED
 keys = keypad.KeyMatrix(
@@ -35,7 +28,7 @@ keys = keypad.KeyMatrix(
 )
 
 
-    
+
 # Simulation loop
 while True:
 
@@ -48,5 +41,17 @@ while True:
             row, col = key_to_pixel_map(key_event.key_number, cols=cols)
 
             print('added at', row, col)
+
+            # the ordering of the cells is different from the LEDs
+            if row < rows // 2:
+                if row % 2 == 1:
+                    col = cols - col - 1
+            else:
+                if row % 2 == 0:
+                    col = cols - col - 1
+                
+            pixel = row * cols + col
+
+            pixels[pixel] = (255, 0, 0)
 
             
