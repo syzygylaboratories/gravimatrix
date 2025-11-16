e066aebebe692ddfbb8681b9790110bbd410483c
stephenmwilkins <s.wilkins@sussex.ac.uk>
2025-11-16 15:13:40 +0000

    updated

--- setup2.sh
+++ setup2.sh
@@ -1,13 +1,10 @@
-# The other script stopped at the venv sourcing bit for some reason
-
-pip3 install --upgrade adafruit-python-shell
+sudo ../venv/bin/pip3 install --upgrade adafruit-python-shell
 wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
 sudo -E env PATH=$PATH python3 raspi-blinka.py
-pip3 install numpy
-pip3 install rpi_ws281x 
-pip3 install adafruit-circuitpython-neopixel
-python3 -m pip install --force-reinstall adafruit-blinka
-
+sudo ../venv/bin/pip3 install numpy
+sudo ../venv/bin/pip3 install rpi_ws281x 
+sudo ../venv/bin/pip3 install adafruit-circuitpython-neopixel
+sudo ../venv/bin/pip3 install --force-reinstall adafruit-blinka
 
 sudo raspi-config nonint do_i2c 0
 sudo raspi-config nonint do_spi 0
@@ -16,4 +13,4 @@ sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint disable_raspi_config_at_boot 0
 
-echo 'sudo syzygy/gravimatrix/venv/bin/python syzygy/gravimatrix/gravimatrix-12x10.py' >> ~/.bashrc
+echo 'sudo syzygy/syzygy/gravimatrix/venv/bin/python3 syzygy/syzygy/gravimatrix/dev/test_buttons-12x10.py' >> ~/.bashrc
